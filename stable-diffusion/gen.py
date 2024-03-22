2e3f437ab3f3932bf342bddbbf0f653b7d0604c8
wyou <taotaozq_02@sina.cn>
2024-03-22 15:28:32 +0800

    python script to generate a picture by text

--- stable-diffusion/gen.py
+++ stable-diffusion/gen.py
@@ -1,15 +1,25 @@
 #!/bin/env python3
 
+import os
+import sys
 from diffusers import AutoPipelineForText2Image
 import torch
 
-pipeline = AutoPipelineForText2Image.from_pretrained(
+def generate(filename, text):
+
+    pipeline = AutoPipelineForText2Image.from_pretrained(
 	"/home/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16"
-).to("cuda")
+    ).to("cuda")
 
-image = pipeline(
-	"stained glass of darth vader, backlight, centered composition, masterpiece, photorealistic, 8k"
-).images[0]
+    image = pipeline(text).images[0]
+    image.save(filename);
 
-image.save("/home/helloworld.png");
+    print(f"Picture Is Successfully Generated !")
 
+
+# Parse command-line arguments.
+if len(sys.argv) != 3:
+    raise ValueError("This script requires exactly two arguments - a filename and some text to write to it.")
+
+# Call the function with the parsed arguments.
+generate(sys.argv[1], sys.argv[2])
